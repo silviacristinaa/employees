33284dd6e6a37e12e52dca5a56e63b730852ad31
silviacristinaa <silviacristinaalexandre1@gmail.com>
2022-12-15 14:38:49 -0300

    [feature/01] - CRUD Funcionários

--- src/main/java/com/github/silviacristinaa/employees/EmployeesApplication.java
+++ src/main/java/com/github/silviacristinaa/employees/EmployeesApplication.java
@@ -9,5 +9,5 @@ public class EmployeesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
 	}
-
+	
 }
---
+++ src/main/java/com/github/silviacristinaa/employees/configs/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.github.silviacristinaa.employees.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
+}
