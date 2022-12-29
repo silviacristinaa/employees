9773d6aaacd94d1eb171d7dd04f49391924fdb86
silviacristinaa <silviacristinaalexandre1@gmail.com>
2022-12-29 20:51:31 -0300

    [feature/06] - Testes Unitários Service e Resource

--- src/main/java/com/github/silviacristinaa/employees/dtos/requests/EmployeeStatusRequestDto.java
+++ src/main/java/com/github/silviacristinaa/employees/dtos/requests/EmployeeStatusRequestDto.java
@@ -1,8 +1,12 @@
 package com.github.silviacristinaa.employees.dtos.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class EmployeeStatusRequestDto {
 	
